e5787b6545823586b567b64b2dc044732ba27620
Mr-Bird-Man <162941632+Mr-Bird-Man@users.noreply.github.com>
2025-07-03 14:08:44 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,7 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
-
-app.get("/home", (req, res) => {
+app.get("/index", (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
